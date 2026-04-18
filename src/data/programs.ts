07f450850ecf4e0b2d02f0b96fb78a9c6e8df8fa
Shuvam Pandey <shuvampandey1@gmail.com>
2026-04-19 01:55:04 +0545

    feat: Add semester filtering for Computer Engineering

---
+++ src/data/programs.ts
@@ -0,0 +1,36 @@
+export const programs = {
+  "Computer Engineering": [
+    {
+      semester: "I/I",
+      subjects: ["MATH101", "PHYS101", "COMP102", "ENGG111", "CHEM101", "EDRG101", "ENGG101"]
+    },
+    {
+      semester: "I/II",
+      subjects: ["MATH104", "PHYS102", "COMP116", "ENGG112", "ENGT105", "ENVE101", "EDRG102", "ENGG102"]
+    },
+    {
+      semester: "II/I",
+      subjects: ["MATH208", "MCSC201", "EEEG202", "EEEG211", "COMP202", "COMP206", "EEEG217"]
+    },
+    {
+      semester: "II/II",
+      subjects: ["MATH207", "MCSC202", "COMP204", "COMP231", "COMP232", "COMP207"]
+    },
+    {
+      semester: "III/I",
+      subjects: ["MGTS301", "COMP307", "COMP315", "COEG304", "COMP310", "COMP303", "COMP301"]
+    },
+    {
+      semester: "III/II",
+      subjects: ["COMP304", "COMP302", "COMP342", "COMP314", "COMP306", "COMP343", "COMP308"]
+    },
+    {
+      semester: "IV/I",
+      subjects: ["MGTS403", "COMP401", "COMP472", "COMP409", "COMP407"]
+    },
+    {
+      semester: "IV/II",
+      subjects: ["MGTS402", "COMP408"]
+    }
+  ]
+};
